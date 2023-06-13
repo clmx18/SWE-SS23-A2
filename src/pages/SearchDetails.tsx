0b0df9a9a18647a8fcc9f1a7fe188765a39a9c5a
Elena Badliuk <bael1018@h-ka.de>
2023-06-13 17:01:20 +0200

    SearchDetails updated

--- src/pages/SearchDetails.tsx
+++ src/pages/SearchDetails.tsx
@@ -1,39 +1,88 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { queryBuch } from "../api/graphql";
-import { Button, Card, CardActions, CardContent, Grid, Rating, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Grid, Rating, Skeleton, Typography } from "@mui/material";
+import WarningAmberOutlinedIcon from '@mui/icons-material/WarningAmberOutlined';
 
 function SearchDetails() {
-  const params = useParams();
-  const [buch, setBuch] = useState({
-    id: '',
-    version: '',
-    isbn: '',
-    rating: '',
-    art: '',
-    preis: '',
-    rabatt: '',
-    lieferbar: '',
-    datum: '',
-    homepage: '',
-    schlagwoerter: '',
-    titel: {
-      titel: '',
-      untertitel:''
-    }
-  });
 
-    useEffect(() => {
-      if(params.id) {
-        queryBuch(params.id).then((buch)=>{
-          if(buch.data.data){
-            setBuch(buch.data.data.buch);
-          }
-        }).catch((err)=>{
-          console.log(err.message);
-        });
+// ====================================================== NEW ==========
+
+useEffect(() => {
+  fetchBuch();
+}, []);
+
+const params = useParams();
+const [isLoading, setIsLoading] = useState(true);
+const [isError, setIsError] = useState(undefined);
+const [buch, setBuch] = useState({
+  id: '',
+  version: '',
+  isbn: '',
+  rating: 0,
+  art: '',
+  preis: 0,
+  rabatt: 0,
+  lieferbar: false,
+  datum: '',
+  homepage: '',
+  schlagwoerter: '',
+  titel: {
+    titel: '',
+    untertitel:''
+  }
+});
+
+const fetchBuch = () => {
+  setIsLoading(true);
+  setIsError(undefined);
+
+  if(params.id) {
+    queryBuch(params.id).then((result) => {
+      setIsLoading(false);
+
+      if (result.data.data.buch) {
+        setBuch(result.data.data.buch);
       }
-    }, []);
+      if (result.data.errors) {
+        const errorString = result.data.errors
+          .flatMap((error: any) => error.message)
+          .toString();
+        setIsError(errorString);
+      }
+    })
+    .catch((err) => {
+      setIsLoading(false);
+      setIsError(err.message);
+    });
+  }
+};
+
+// ============================================== OLD ============
+    // useEffect(() => {
+    //   if(params.id) {
+    //     queryBuch(params.id).then((buch)=>{
+    //       if(buch.data.data){
+    //         setBuch(buch.data.data.buch);
+    //       }
+    //     }).catch((err)=>{
+    //       console.log(err.message);
+    //     });
+    //   }
+    // }, []);
+
+
+
+  function decimalToPercentage(decimal: number): string {
+    const percentage = (decimal * 100).toFixed(2);
+    return percentage + " %";
+  }
+
+  function convertToPrice(amount: number): string {
+    const formatter = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' });
+    const formattedPrice = formatter.format(amount);
+    return formattedPrice;
+  }
 
   return (
     <>
@@ -175,19 +224,70 @@ function SearchDetails() {
       </Card>
   </div>
   </Grid>
-  </>
+
+  <Grid item md={9}>
+          <div style={{ padding: '3rem' }}>
+          {isLoading === true ? (
+            <div>
+          <CardActions style={{ justifyContent: 'start' }}>
+              <Skeleton variant="rounded" width={120} height={30} />
+              </CardActions>
+              <Card
+                style={{
+                  textAlign: 'left',
+                  marginBottom: '2rem',
+                  paddingLeft: '1rem',
+                }}
+              >
+                <CardContent>
+                  <Skeleton variant="text" sx={{ fontSize: '2rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '2rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                  <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                </CardContent>
+              </Card>
+              </div>
+            ) : (
+              false
+            )}
+            {isError !== undefined ? (
+              <Card
+                style={{
+                  textAlign: 'center',
+                  marginBottom: '2rem',
+                  paddingLeft: '1rem',
+                }}
+              >
+                <CardContent>
+                  <WarningAmberOutlinedIcon fontSize="large" />
+                  <Typography gutterBottom variant="h5" component="div">
+                    Keine Suchergebnisse gefunden
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant="body1"
+                    component="div"
+                    style={{ color: 'red', marginTop: '1rem' }}
+                  >
+                    {isError}
+                  </Typography>
+                </CardContent>
+              </Card>
+            ) : (
+              false
+            )}
+          </div>
+        </Grid>
+    </>
   );
-  }
-
-  function decimalToPercentage(decimal: number): string {
-    const percentage = (decimal * 100).toFixed(2);
-    return percentage + " %";
-  }
-
-  function convertToPrice(amount: number): string {
-    const formatter = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' });
-    const formattedPrice = formatter.format(amount);
-    return formattedPrice;
-  }
+}
 
 export default SearchDetails;
